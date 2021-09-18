05808187ac124156b8d89b8cd75385108476abdf
Shane Rex <shanerex2001@gmail.com>
2021-09-18 18:44:55 +0530

    Added endpoints for HTML Files

--- app.py
+++ app.py
@@ -10,4 +10,24 @@ def home():
 def courses():
   return render_template("courses.html")
 
+@app.route("/courseshome")
+def courses_home():
+  return render_template("course_home.html")
+
+@app.route("/newsletter")
+def newsletter():
+  return render_template("newsletter.html")
+
+@app.route("/player")
+def player():
+  return render_template("player.html")
+
+@app.route("/privacy")
+def privacy():
+  return render_template("privacy.html")
+
+@app.route("/register")
+def register():
+  return render_template("register.html")
+
 app.run()
